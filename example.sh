8df9cb1d047689530fa9545b7a8b4ba9979cc5c4
Jon Higgs <jhiggs@eml.cc>
2015-11-20 13:03:45 +1100

    Start an example

---
+++ example.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+source "$(dirname $0)/lib/pure-getopt-plus.inc"
+
+pgop_description "this is an example application"
+pgop_add global n name    "Set your name"
+pgop_add global v version "Display the version information"
+
+pgop_help global
+#pgop_context_add "global"
+
+#pgop_setup $@     # we may be able to get away with not doing this
+#
+#pgop_set --name "the default name"
+#
+#function version() {
+#    echo "version 1.0.0"
+#    return 0
+#}
+#
+#function name() {
+#    name="$(pgop_value --name)"
+#    echo "my name is ${name}"
+#    return 0
+#}
+#
+#for arg in ${pgop_options}; do
+#    case $arg in
+#        "-n"|"--name")
+#            pgop_set --name $(pgop_value ${arg})
+#            name
+#        ;;
+#        "-v"|"--version") version ;;
+#    esac
+#done
