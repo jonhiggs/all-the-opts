24092aef0eac39b5a091415c6b35882d6b40ba3c
Jon Higgs <jhiggs@eml.cc>
2015-11-20 16:30:59 +1100

    Clean up the example more

--- example.sh
+++ example.sh
@@ -2,42 +2,18 @@
 source "$(dirname $0)/lib/pure-getopt-plus.inc"
 
 pgop_description          "This is an example application"
+pgop_add global h  help   "Set your name"
 pgop_add global n: name:  "Set your name"
 pgop_add global v version "Display the version information"
 
+pgop_setup global $@ || exit 1
 
-__pgop_getopt_short
-__pgop_getopt_long
+echo "operands are:"
+pgop_operands
 
-for o in $@; do
-  if [[ $o == '--help' ]]; then
-    pgop_help global
-    exit 0
-  fi
-done
+pgop_argument global version && echo "version is set"
 
-
-#pgop_setup $@     # we may be able to get away with not doing this
-#
-#pgop_set --name "the default name"
-#
-#function version() {
-#    echo "version 1.0.0"
-#    return 0
-#}
-#
-#function name() {
-#    name="$(pgop_value --name)"
-#    echo "my name is ${name}"
-#    return 0
-#}
-#
-#for arg in ${pgop_options}; do
-#    case $arg in
-#        "-n"|"--name")
-#            pgop_set --name $(pgop_value ${arg})
-#            name
-#        ;;
-#        "-v"|"--version") version ;;
-#    esac
-#done
+name="$(pgop_argument global name)"
+if [[ ! -z ${name} ]]; then
+  echo "name is ${name}"
+fi
