a2e93cc58c315a29cf66207a737b3c188781be77
Jon Higgs <jhiggs@eml.cc>
2015-11-24 19:26:18 +1100

    Renames in the example

--- example.sh
+++ example.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 source "$(dirname $0)/lib/all-the-opts.inc"
-pgop_context "global"
-pgop_description          "This is an example application"
+ato_context "global"
+ato_description          "This is an example application"
 
 #          CONTEXT      SHORT  LONG      DESCRIPTION
-#pgop_add   n:     name:     "Set your name"
-#pgop_read
-#pgop_add   v      version   "Display the version information"
+#ato_add   n:     name:     "Set your name"
+#ato_read
+#ato_add   v      version   "Display the version information"
 
-pgop_add   h      help      "Set your name"
-pgop_read
-#pgop_setup $@ || exit 0
+ato_add   h      help      "Set your name"
+ato_read
+#ato_setup $@ || exit 0
 
-#for operand in pgop_operands; do
+#for operand in ato_operands; do
 #  echo "found an operand of ${operand}"
 #done
 #
-#pgop_argument global version && echo "version is set"
+#ato_argument global version && echo "version is set"
 #
-#name="$(pgop_argument global name)"
+#name="$(ato_argument global name)"
 #if [[ ! -z ${name} ]]; then
 #  echo "name is ${name}"
 #fi
